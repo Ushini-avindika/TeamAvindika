046cd7a87824496849aab8dd070b850ed5e79c1a
Madush99 <madusankagajadeera99@gmail.com>
2021-07-02 01:38:49 +0530

    cb

--- frontend/src/Screens/HomeScreen/homescreen.js
+++ frontend/src/Screens/HomeScreen/homescreen.js
@@ -17,7 +17,6 @@ const homeScreen = () => {
                         <Appbar />
                         <Showcase />
                         <NewsList />
-
                         <Footer />
 
                   </div>
