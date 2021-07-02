7abdf792b99a7d023fcbf9ce7923bb7eaec196f0
davebulner <davebulner3@gmail.com>
2021-07-03 00:11:39 +0545

    payment atendee

---
+++ frontend/src/Screens/atendeeScreen/PayPal.js
@@ -0,0 +1,38 @@
+import React, { useRef, useEffect } from 'react'
+
+export default function PayPal() {
+    const paypal = useRef()
+
+    useEffect(() => {
+        window.paypal.Buttons({
+            createOrder: (data, actions, err) => {
+                return actions.order.create({
+
+                    intent: "CAPTURE",
+                    purchase_units: [
+                        {
+                            description: "Conference Upfront",
+                            amount: {
+                                currency_code: "USD",
+                                value: 100.00,
+                            }
+                        }
+                    ]
+                })
+
+            },
+            onApprove: async (data, actions) => {
+                const order = await actions.order.capture()
+                console.log(order)
+            },
+            onError: (err) => {
+                console.log(err)
+            }
+        }).render(paypal.current)
+    }, [])
+    return (
+        <div>
+            <div ref={paypal}></div>
+        </div>
+    )
+}
