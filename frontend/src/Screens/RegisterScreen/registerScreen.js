1c5eecb60c0698bc2a3a248f1522599cdec577e2
Madush99 <madusankagajadeera99@gmail.com>
2021-07-03 04:42:50 +0530

    make it better

--- frontend/src/Screens/RegisterScreen/registerScreen.js
+++ frontend/src/Screens/RegisterScreen/registerScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Button, Form, Row, Col } from 'react-bootstrap'
-import './registerscreen.css'
+import '../RegisterScreen/registerscreen.css'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import Navbar from '../../components/Navbar/navbar.js'
