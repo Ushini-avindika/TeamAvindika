ded07965036fd5d6a76e5c05e7b6d10466abe574
Madush99 <madusankagajadeera99@gmail.com>
2021-07-03 10:17:56 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,12 @@ connectDB()
 
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+    origin: 'http://localhost:5000',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 app.use(express.json())
 
 app.use((req, res, next) => {
