ca1059b9d821ea2f6b432bb3699a6a11678aa6d4
Madush99 <madusankagajadeera99@gmail.com>
2021-07-03 06:01:03 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -63,6 +63,6 @@ if (process.env.NODE_ENV == 'production') {
     })
 }
 
-const PORT = process.env.PORT || 8040
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`server running on port ${PORT}`.brightYellow))
