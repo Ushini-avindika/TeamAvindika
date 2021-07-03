b93fd41af604d9cb55f5e537b1266cea843003a0
Madush99 <madusankagajadeera99@gmail.com>
2021-07-03 10:23:33 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,7 @@ connectDB()
 
 const app = express()
 
-const corsOptions = {
-    origin: 'http://localhost:5000',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+app.use(cors())
 app.use(express.json())
 
 app.use((req, res, next) => {
@@ -68,6 +63,6 @@ if (process.env.NODE_ENV == 'production') {
     })
 }
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8040
 
 app.listen(PORT, console.log(`server running on port ${PORT}`.brightYellow))
